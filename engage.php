650d0acf3ea262adffc4742ddbea986afbeb70b1
Aishwarya <aishwarya923@gmail.com>
2014-09-23 23:23:32 +0530

    Removed theme button.

--- engage.php
+++ engage.php
@@ -34,32 +34,6 @@
 </head> 
 
 <body>
-<!--=== Style Switcher ===-->    
-<i class="style-switcher-btn fa fa-cogs hidden-xs"></i>
-<div class="style-switcher animated fadeInRight">
-    <div class="theme-close"><i class="icon-close"></i></div>
-    <div class="theme-heading">Theme Colors</div>
-    <ul class="list-unstyled">
-        <li class="theme-default theme-active" data-style="default" data-header="light"></li>
-        <li class="theme-blue" data-style="blue" data-header="light"></li>
-        <li class="theme-orange" data-style="orange" data-header="light"></li>
-        <li class="theme-red" data-style="red" data-header="light"></li>
-        <li class="theme-light last" data-style="light" data-header="light"></li>
-
-        <li class="theme-purple" data-style="purple" data-header="light"></li>
-        <li class="theme-aqua" data-style="aqua" data-header="light"></li>
-        <li class="theme-brown" data-style="brown" data-header="light"></li>
-        <li class="theme-dark-blue" data-style="dark-blue" data-header="light"></li>
-        <li class="theme-light-green last" data-style="light-green" data-header="light"></li>
-    </ul>
-    <div style="margin-bottom:18px;"></div>
-    <div class="theme-heading">Layouts</div>
-    <div class="text-center">
-        <a href="javascript:void(0);" class="btn-u btn-u-green btn-block active-switcher-btn wide-layout-btn">Wide</a>
-        <a href="javascript:void(0);" class="btn-u btn-u-green btn-block boxed-layout-btn">Boxed</a>
-    </div>
-</div><!--/style-switcher-->
-<!--=== End Style Switcher ===-->    
 
 <div class="wrapper">
 
@@ -207,6 +181,13 @@
                             <textarea rows="8" class="form-control" name="message"></textarea>
                         </div>                
                     </div>
+
+                    <label>2 + 2 = ?</label>
+                    <div class="row margin-bottom-20">
+                        <div class="col-md-2 col-md-offset-0">
+                            <input type="text" class="form-control" name="human_verify">
+                        </div>                
+                    </div>
                     
                     <p><button type="submit" class="btn-u" name="submit">Send Message</button></p>
                 </form>
@@ -220,7 +201,10 @@
                             $mailcheck = spamcheck($_POST["from"]);
                             if ($mailcheck==FALSE) {
                               echo "Please validate your Email ID";
-                            } 
+                            }
+                            elseif ($_POST["human_verify"] != 4) {
+                                echo "Invalid answer";
+                            }
                             else {
                               $username = $_POST["username"]; // sender
                               $from = $_POST["from"]; // sender
